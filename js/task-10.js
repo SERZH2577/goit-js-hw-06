dcc8314389c7008627f96aff19144679c77404f8
Serhii Plis <serzh2577@gmail.com>
2022-01-13 12:52:36 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+const btnCreate = document.querySelector('[data-create]')
+const dtnDestroy = document.querySelector('[data-destroy]')
+const boxEl = document.querySelector('#boxes')
+const controlElInput = document.querySelector('#controls input')
+
+btnCreate.addEventListener('click', createBoxes)
+dtnDestroy.addEventListener('click', destroyBoxes)
+
+function createBoxes() {
+	let box = 20
+	for (let i = 0; i < Number(controlElInput.value); i += 1) {
+		const createDiv = document.createElement('div')
+		box += 10
+		createDiv.style.backgroundColor = getRandomHexColor()
+		createDiv.style.width = `${box}px`
+		createDiv.style.height = `${box}px`
+		boxEl.append(createDiv)
+	}
+}
+
+function destroyBoxes() {
+	controlElInput.value = ''
+	boxEl.innerHTML = ''
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
